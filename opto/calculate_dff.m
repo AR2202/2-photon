c8c25c1f31e2ddf767e92d0b5b2c292b114f94e4
Annika Rings <annika.rings@cncb.ox.ac.uk>
2020-01-14 16:31:22 +0000

    deleted unused files

--- opto/calculate_dff.m
+++
@@ -1,8 +0,0 @@
-function dff = calculate_dff(f,baseline_start,baseline_end)
-         f0=mean(f(:,baseline_start:baseline_end),2); %calculate baseline between baseline_start and baseline_end
-         no_frame = numel(f);
-         df = f-f0;
-         dff=df./f0;
-               
-                
-            
