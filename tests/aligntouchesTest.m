cb7a23fb0e3eedfdf0a104080151919106ba07cb
Annika Rings <annika.rings@cncb.ox.ac.uk>
2020-01-17 11:17:41 +0000

    added additional tests

--- tests/aligntouchesTest.m
+++ tests/aligntouchesTest.m
@@ -19,3 +19,21 @@ verifyEqual(testCase,actSolution_mean,expSolution_mean);
 verifyEqual(testCase,actSolution_SEM,expSolution_SEM);
 
 end
+
+%this test currently fails - function align_touches can't handle missing data
+%needs to be updated
+
+function test_align_touches_missing_data(testCase)
+load(fullfile('testdata/mean_male_touch.mat'),'male_mean_event_m','male_SEM_event_m');
+expSolution_mean=male_mean_event_m;
+expSolution_SEM=male_SEM_event_m;
+
+
+align_touches('touchdir','touchtimes_missing_data','resultsdir','Results_test','outputdirmean','Results_test','reduced',true);
+load(fullfile('Results_test/mean_male_touch.mat'),'male_mean_event_m','male_SEM_event_m');
+actSolution_mean=male_mean_event_m;
+actSolution_SEM=male_SEM_event_m;
+verifyEqual(testCase,actSolution_mean,expSolution_mean);
+verifyEqual(testCase,actSolution_SEM,expSolution_SEM);
+
+end
