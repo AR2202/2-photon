5880fb9c4ac825506de99cd1ca4f14295b74b4ad
Annika Rings <annika.rings@cncb.ox.ac.uk>
2020-02-17 14:23:23 +0000

    added tests for female experimental flies

--- tests/aligntouchesTest.m
+++ tests/aligntouchesTest.m
@@ -36,6 +36,21 @@ verifyNotEqual(testCase,actSolution_SEM,expSolution_SEM);
 
 end
 
+
+function test_align_touches_female(testCase)
+load(fullfile('testdata/mean_female_touch.mat'),'male_mean_event_f','male_SEM_event_f');
+expSolution_mean=male_mean_event_f; 
+expSolution_SEM=male_SEM_event_f;
+
+
+align_touches('touchdir','touchtimes_mock_reduced','resultsdir','Results_test','outputdirmean','Results_test');
+load(fullfile('Results_test/mean_female_touch.mat'),'male_mean_event_f','male_SEM_event_f');
+actSolution_mean=male_mean_event_f;
+actSolution_SEM=male_SEM_event_f;
+verifyEqual(testCase,actSolution_mean,expSolution_mean);
+verifyEqual(testCase,actSolution_SEM,expSolution_SEM);
+
+end
 %this test currently fails - function align_touches can't handle missing data
 %needs to be updated
 
