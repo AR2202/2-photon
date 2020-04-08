f547ed2a7a22c600566a47d347bd86b29b6b728f
Annika Rings <annika.rings@cncb.ox.ac.uk>
2020-04-08 09:25:49 +0100

    chanded dff_pulses calculation to account for shorter pulses

--- common/dff_pulses.m
+++ common/dff_pulses.m
@@ -1,8 +1,8 @@
  function pulse_dff=dff_pulses(fluo, pulsetimes,pulsedur,framerate)
         for i=1:numel(pulsetimes)
             start_event=round(pulsetimes(i)*framerate);
-            end_event = round(pulsetimes(i)*framerate+pulsedur*framerate);
-            start_base=round(pulsetimes(i)*framerate-pulsedur*framerate);
+            end_event = round(pulsetimes(i)*framerate+pulsedur*framerate)+1;
+            start_base=round(pulsetimes(i)*framerate-pulsedur*framerate)-1;
             end_base=round(pulsetimes(i)*framerate)-1;
             pulse_f0=mean(fluo(start_base:end_base));
             pulse_f=max(fluo(start_event:end_event));
