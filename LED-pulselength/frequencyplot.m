c56e4b5bc7c062937e68ec79853f9450049a2da1
Annika Rings <annika.rings@cncb.ox.ac.uk>
2020-07-29 17:43:45 +0100

    added inhibitor concentration

--- LED-pulselength/frequencyplot.m
+++ LED-pulselength/frequencyplot.m
@@ -40,6 +40,9 @@ options = struct('framerate',5.92,'baseline_start',2,'baseline_end',11,...
     'frequencies',[4,10,20,40], 'pulselengths',[8,12,20],...
     'pulsetimes',[20,40,60,80],'genders',{{'_male';'female'}},...
     'neuronparts',{{'medial';'lateral'}},'pulsedur', 5,...
+    'inhibitor_conc',[0,150,300],...
+    'inhibitor_unit','uM',...
+    'inhibitor_name','PTX',...
     'resultsdir','Results','multiroi',false,'numrois',1,...
     'pulsetimesfromfile',false,'pulsetimesfile','stimtimes.mat','subfoldername','ROI');
 arguments = varargin;
@@ -68,6 +71,9 @@ multiroi=options.multiroi;
 numrois=options.numrois;
 pulsetimesfromfile=options.pulsetimesfromfile;
 pulsetimesfile=options.pulsetimesfile;
+inhibconc=options.inhibitor_conc;
+inhibname=options.inhibitor_name;
+inhibunit=options.inhibitor_unit;
 if pulsetimesfromfile
     load(pulsetimesfile);
     pulsetimes = stimstarts./framerate;
@@ -116,121 +122,131 @@ for g=1:size(genders,1)
     disp(gender);
     for n=1:size(neuronparts,1)
         neuronpart=neuronparts{n};
-        
-        for j = 1:length(pulselengths)
-            pulselengthname=strcat(string(pulselengths(j)),'ms_');
-            disp(pulselengthname);
-            for i = 1:length(frequencies)
-                f = string(frequencies(i));
-                outputname = strcat(pulselengthname,f,'Hz');
-                filenames=[];
-                
-                for p = 1:numel(directories)
-                    if ~directories(p).isdir
-                        continue;
+        for inhib = 1:length(inhibconc)
+            inhibstring = strcat(string(inhibconc(inhib)),inhibunit,inhibname);
+            disp(inhibstring);
+            
+            for j = 1:length(pulselengths)
+                pulselengthname=strcat(string(pulselengths(j)),'ms_');
+                disp(pulselengthname);
+                for i = 1:length(frequencies)
+                    f = string(frequencies(i));
+                    outputname = strcat(pulselengthname,f,'Hz');
+                    all_filenames=[];
+                    
+                    
+                    for p = 1:numel(directories)
+                        if ~directories(p).isdir
+                            continue;
+                        end
+                        if ismember(directories(p).name,{'.','..','.DS_Store'})
+                            continue;
+                        end
+                        if issubdir
+                            directoryname=pathname;
+                        else
+                            directoryname = fullfile(pathname,foldername,directories(p).name);
+                            disp(directoryname);
+                        end
+                        files=dir(char(strcat(directoryname,'/',subfoldername,'/*',gender,'*',neuronpart,'*',pulsedurstr,'*',f,'Hz*',pulselengthname,'*.tif')));
+                        newfilenames=arrayfun(@(f) fullfile(directoryname,subfoldername,f.name),files, 'uni',false);
+                        all_filenames = vertcat(all_filenames,newfilenames);
+                        if inhibconc(inhib) ==0
+                            filenames = all_filenames(~contains(all_filenames,inhibname));
+                        else
+                            filenames = all_filenames(contains(all_filenames,inhibstring));
+                        end
+                        disp(filenames);
                     end
-                    if ismember(directories(p).name,{'.','..','.DS_Store'})
-                        continue;
+                    fluo=cellfun(@(filename)extract_fluo(filename),filenames,'uni',false);
+                    fluomat=cell2mat(fluo);
+                    pulseaverage_dff=cellfun(@(f) average_pulses(f,pulsetimes,framerate),fluo,'uni',false);
+                    pulseavmat=cell2mat(pulseaverage_dff);
+                    
+                    firstpulse_dff=cellfun(@(f) average_pulses(f,pulsetimes(1),framerate),fluo,'uni',false);
+                    firstmat=cell2mat(firstpulse_dff);
+                    
+                    lastpulse_dff=cellfun(@(f) average_pulses(f,pulsetimes(size(pulsetimes,2)),framerate),fluo,'uni',false);
+                    lastmat=cell2mat(lastpulse_dff);
+                    
+                    
+                    %calculating deltaF/F
+                    dff=calculate_dff(fluomat,baseline_start,baseline_end);
+                    numframes = size(dff,2);
+                    
+                    framenumbers=1:numframes;
+                    x=framenumbers/framerate;
+                    
+                    %calculating mean, n and SEM of dff
+                    mean_dff=mean(dff);
+                    n_files=size(dff,1)/numrois;
+                    n_rois=size(dff,1);
+                    SEM_dff=std(dff)./n_files;
+                    mean_pulseav_dff=mean(pulseavmat,1);
+                    SEM_pulseav_dff=std(pulseavmat)./n_files;
+                    mean_first_dff=mean(firstmat,1);
+                    SEM_first_dff=std(firstmat)./n_files;
+                    mean_last_dff=mean(lastmat,1);
+                    SEM_last_dff=std(lastmat)./n_files;
+                    %make the figure name and path to output
+                    
+                    
+                    outputfig=fullfile(outputdir,(strcat(outputname,gender,'_',neuronpart,'_',pulsedurstr,inhibstring,'.eps')));
+                    fignew=figure('Name',strcat( outputname,gender,'_',neuronpart,'_',pulsedurstr,inhibstring));
+                    %plot the mean with a shaded area showing the SEM
+                    h=boundedline(x,mean_dff, SEM_dff,'k');
+                    xlim([0,105]);
+                    ylim([-0.2,0.8])
+                    for numpatch = 1:size(pulsetimes,2)%loop through the number of pulses
+                        %for each pulse, generate a shaded area in the plot
+                        %indicating the pulse duration
+                        hpatch(numpatch)=patch([pulsetimes(numpatch) pulsetimes(numpatch)+pulsedur pulsetimes(numpatch)+pulsedur pulsetimes(numpatch)] , [min(ylim)*[1 1] max(ylim)*[1 1]],'r','EdgeColor','none');
+                        %send the shaded area to the back of the graph
+                        uistack(hpatch(numpatch), 'bottom');
                     end
-                    if issubdir
-                        directoryname=pathname;
-                    else
-                        directoryname = fullfile(pathname,foldername,directories(p).name);
-                        disp(directoryname);
-                    end
-                    files=dir(char(strcat(directoryname,'/',subfoldername,'/*',gender,'*',neuronpart,'*',pulsedurstr,'*',f,'Hz*',pulselengthname,'*.tif')));
-                    newfilenames=arrayfun(@(f) fullfile(directoryname,subfoldername,f.name),files, 'uni',false);
-                    filenames = vertcat(filenames,newfilenames);
-                    disp(filenames);
-                end
-                fluo=cellfun(@(filename)extract_fluo(filename),filenames,'uni',false);
-                fluomat=cell2mat(fluo);
-                pulseaverage_dff=cellfun(@(f) average_pulses(f,pulsetimes,framerate),fluo,'uni',false);
-                pulseavmat=cell2mat(pulseaverage_dff);
-                
-                firstpulse_dff=cellfun(@(f) average_pulses(f,pulsetimes(1),framerate),fluo,'uni',false);
-                firstmat=cell2mat(firstpulse_dff);
-                
-                lastpulse_dff=cellfun(@(f) average_pulses(f,pulsetimes(size(pulsetimes,2)),framerate),fluo,'uni',false);
-                lastmat=cell2mat(lastpulse_dff);
-                
-                
-                %calculating deltaF/F
-                dff=calculate_dff(fluomat,baseline_start,baseline_end);
-                numframes = size(dff,2);
-                
-                framenumbers=1:numframes;
-                x=framenumbers/framerate;
-                
-                %calculating mean, n and SEM of dff
-                mean_dff=mean(dff);
-                n_files=size(dff,1)/numrois;
-                n_rois=size(dff,1);
-                SEM_dff=std(dff)./n_files;
-                mean_pulseav_dff=mean(pulseavmat,1);
-                SEM_pulseav_dff=std(pulseavmat)./n_files;
-                mean_first_dff=mean(firstmat,1);
-                SEM_first_dff=std(firstmat)./n_files;
-                mean_last_dff=mean(lastmat,1);
-                SEM_last_dff=std(lastmat)./n_files;
-                %make the figure name and path to output
-                
-                
-                outputfig=fullfile(outputdir,(strcat(outputname,gender,'_',neuronpart,'_',pulsedurstr,'.eps')));
-                fignew=figure('Name',strcat( outputname,gender,'_',neuronpart,'_',pulsedurstr));
-                %plot the mean with a shaded area showing the SEM
-                h=boundedline(x,mean_dff, SEM_dff,'k');
-                xlim([0,105]);
-                ylim([-0.2,0.8])
-                for numpatch = 1:size(pulsetimes,2)%loop through the number of pulses
-                    %for each pulse, generate a shaded area in the plot
-                    %indicating the pulse duration
-                    hpatch(numpatch)=patch([pulsetimes(numpatch) pulsetimes(numpatch)+pulsedur pulsetimes(numpatch)+pulsedur pulsetimes(numpatch)] , [min(ylim)*[1 1] max(ylim)*[1 1]],'r','EdgeColor','none');
+                    outputfig2=fullfile(outputdir,(strcat(outputname,gender,'_',neuronpart,'_',pulsedurstr,inhibstring,'mean_pulse.eps')));
+                    fignew2=figure('Name',strcat( outputname,gender,'_',neuronpart,'_',pulsedurstr,inhibstring,'_mean_pulse'));
+                    %plot the mean with a shaded area showing the SEM
+                    x2=1:180;
+                    h=boundedline(x2,(transpose(mean_pulseav_dff)), transpose(SEM_pulseav_dff),'k');
+                    xlim([0,180]);
+                    ylim([-0.2,0.8])
+                    
+                    hpatch2=patch([61 91 91 61] , [min(ylim)*[1 1] max(ylim)*[1 1]],'r','EdgeColor','none');
                     %send the shaded area to the back of the graph
-                    uistack(hpatch(numpatch), 'bottom');
+                    uistack(hpatch2, 'bottom');
+                    
+                    saveas(fignew2,outputfig2,'epsc');
+                    dff_of_pulses=cellfun(@(f)dff_pulses(f,pulsetimes,pulsedur,framerate),fluo,'uni',false);
+                    
+                    pulsedff=cell2mat(dff_of_pulses);
+                    outputmatfile=fullfile(outputdir,(strcat(outputname,gender,'_',neuronpart,'_',pulsedurstr,inhibstring,'.mat')));
+                    save(outputmatfile,'pulsedff','mean_dff','n_files','SEM_dff','dff','mean_pulseav_dff', 'SEM_pulseav_dff','mean_first_dff','SEM_first_dff','mean_last_dff','SEM_last_dff');
+                    pulsemeans(j,i)=mean(pulsedff);
+                    pulseSEMs(j,i)=std(pulsedff)/(numel(pulsedff));
                 end
-                outputfig2=fullfile(outputdir,(strcat(outputname,gender,'_',neuronpart,'_',pulsedurstr,'mean_pulse.eps')));
-                fignew2=figure('Name',strcat( outputname,gender,'_',neuronpart,'_',pulsedurstr,'_mean_pulse'));
-                %plot the mean with a shaded area showing the SEM
-                x2=1:180;
-                h=boundedline(x2,(transpose(mean_pulseav_dff)), transpose(SEM_pulseav_dff),'k');
-                xlim([0,180]);
-                ylim([-0.2,0.8])
-                
-                hpatch2=patch([61 91 91 61] , [min(ylim)*[1 1] max(ylim)*[1 1]],'r','EdgeColor','none');
-                %send the shaded area to the back of the graph
-                uistack(hpatch2, 'bottom');
-                
-                saveas(fignew2,outputfig2,'epsc');
-                dff_of_pulses=cellfun(@(f)dff_pulses(f,pulsetimes,pulsedur,framerate),fluo,'uni',false);
-                
-                pulsedff=cell2mat(dff_of_pulses);
-                outputmatfile=fullfile(outputdir,(strcat(outputname,gender,'_',neuronpart,'_',pulsedurstr,'.mat')));
-                save(outputmatfile,'pulsedff','mean_dff','n_files','SEM_dff','dff','mean_pulseav_dff', 'SEM_pulseav_dff','mean_first_dff','SEM_first_dff','mean_last_dff','SEM_last_dff');
-                pulsemeans(j,i)=mean(pulsedff);
-                pulseSEMs(j,i)=std(pulsedff)/(numel(pulsedff));
             end
+            
+            outputplot=fullfile(outputdir,strcat(gender,'_',neuronpart,inhibstring,'frequencyplot.eps'));
+            fignew=figure('Name',strcat('dF/F vs. frequency',gender,'_',neuronpart,'_',pulsedurstr,inhibstring));
+            colors=['k','r','b','g'];
+            hold 'on';
+            for j=1:size(pulsemeans,1)
+                errorbar(frequencies,pulsemeans(j,:),pulseSEMs(j,:),'o','color',colors(j),'MarkerSize',10);
+            end
+            
+            xlim([0,100]);
+            ylim([0,2.5]);
+            title('Frequency');
+            xlabel('frequency(Hz)');
+            ylabel ('\DeltaF/F');
+            ax = gca;
+            ax.FontSize = 13;
+            ax.LineWidth=2;
+            hold 'off';
+            saveas(fignew,outputplot,'epsc');
+            cd (startdir);
         end
-        
-        outputplot=fullfile(outputdir,strcat(gender,'_',neuronpart,'frequencyplot.eps'));
-        fignew=figure('Name',strcat('dF/F vs. frequency',gender,'_',neuronpart,'_',pulsedurstr));
-        colors=['k','r','b','g'];
-        hold 'on';
-        for j=1:size(pulsemeans,1)
-            errorbar(frequencies,pulsemeans(j,:),pulseSEMs(j,:),'o','color',colors(j),'MarkerSize',10);
-        end
-        
-        xlim([0,100]);
-        ylim([0,2.5]);
-        title('Frequency');
-        xlabel('frequency(Hz)');
-        ylabel ('\DeltaF/F');
-        ax = gca;
-        ax.FontSize = 13;
-        ax.LineWidth=2;
-        hold 'off';
-        saveas(fignew,outputplot,'epsc');
-        cd (startdir);
     end
 end
 
