7b66d1656bdfe6c9b879cb3ab4a191c6dd382fc4
Annika Rings <annika.rings@cncb.ox.ac.uk>
2020-08-07 15:51:29 +0100

    changed the pulse averaging function

--- LED-pulselength/frequencyplot.m
+++ LED-pulselength/frequencyplot.m
@@ -270,12 +270,14 @@ end
 %This creates events of 180 frames
 function pulseav_dff=average_pulses(fluo, pulsetimes,framerate)
 pulsefluo=zeros(size(pulsetimes,2),180);
+pulsefluo_dff=zeros(size(pulsetimes,2),180);
 for numpulse = 1:size(pulsetimes,2)
     pulseframe=floor(pulsetimes(numpulse)*framerate);
     pulsefluo(numpulse,1:180)=fluo((pulseframe-60):(pulseframe+119));
+    pulsefluo_dff(numpulse,1:180)=calculate_dff(pulsefluo(numpulse,1:180),2,58);
 end
-mean_pulsefluo=mean(pulsefluo,1);
-pulseav_dff=calculate_dff(mean_pulsefluo,2,58);
+
+pulseav_dff=mean(pulsefluo_dff,1);
 end
 %function for checking if data are from the same animal
 function average_event=average_within_fly(directorynames,flynumbers,fluo,directoryname1,flynumber1)
