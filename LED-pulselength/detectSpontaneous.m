4d26977683cf996a8260053c994dce02b78f02fc
Annika Rings <annika.rings@cncb.ox.ac.uk>
2020-10-01 17:44:40 +0100

    changed spontaneous detection script

--- LED-pulselength/detectSpontaneous.m
+++ LED-pulselength/detectSpontaneous.m
@@ -1,32 +1,48 @@
-function event = detectSpontaneous(filename) 
+function [freqs,ampls] = detectSpontaneous(filename,varargin) 
+%------------------------------------------------------------------------
+%optional key-value pare arguments and their defaults
+%------------------------------------------------------------------------
+options = struct('threshold',0.1);
+arguments = varargin;
+%------------------------------------------------------------------------
+%setting optional key-value pair arguments
+%-------------------------------------------------------------------------
+
+%call the options_resolver function to check optional key-value pair
+%arguments
+[options,~]=options_resolver(options,arguments,'detectSpontaneous');
+threshold = options.threshold; 
 load(filename, 'dff');
  dffcell = num2cell(dff,2);
- gliding_av_dff_all=movmean(dff(:,:),10);
- shifted = circshift(gliding_av_dff_all,10);
- shifted(:,1:10)=0;
+ gliding_av_dff_all=movmean(dff(:,:),2);
+ shifted = circshift(gliding_av_dff_all,2);
+ shifted(:,1:2)=0;
  difference = gliding_av_dff_all - shifted;
- diffcell=num2cell(difference,2);
- event = cellfun (@(diff) find(diff>0.05), diffcell, 'UniformOutput', false);
- aboveth = difference >0.1;
+ aboveth = difference >threshold;
  edges = diff(aboveth,1,2);
  edgecell=num2cell(edges,2);
  eventstarts = cellfun (@(edge) find(edge==1), edgecell, 'UniformOutput', false);
  freqs = cellfun (@(exp) size(exp,2)/101, eventstarts, 'UniformOutput', false);
- plotevents = cellfun(@(dff1,eventstarts1) plotEvents(dff1,eventstarts1), dffcell, eventstarts, 'UniformOutput', false);
+ampls = cellfun(@(dff1,eventstarts1) plotEvents(dff1,eventstarts1), dffcell, eventstarts, 'UniformOutput', false);
  
  
- function events= plotEvents(dff,starts)
-events=zeros(size(starts,2),18);
+ function ampl= plotEvents(dff,starts)
+maxevents = [];
 figure('Name', 'events');
 hold 'on';
 for start = 1:size(starts,2)
-    startpoint = min(max(starts(start)-6,1),size(dff,2)-17);
+    startpoint = min(max(starts(start)-3,1),size(dff,2)-17);
     endpoint = startpoint +17;
     event = dff(startpoint:endpoint);
     plot(event);
-    events(start,:)=event;
+    
+    maxevent = max(event) - event(1);
+    
+    maxevents(start) = maxevent;
     
 end
 hold 'off';
+
+ampl = mean(maxevents,2);
  
  
