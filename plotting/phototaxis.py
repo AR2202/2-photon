aac554820e610d486e55282dca3eacf78f1c9271
Annika Rings <annika.rings@cncb.ox.ac.uk>
2020-12-04 16:40:24 +0000

    added stats to plotting of phototaxis

--- plotting/phototaxis.py
+++ plotting/phototaxis.py
@@ -15,29 +15,25 @@ dataA = scipy.io.loadmat(file_A, matlab_compatible=True)
 
 PImeansA = dataA["PImeans"][0]
 PIA = np.asarray([data[0] for data in dataA["PI"]])
-print(PImeansA)
-print(PIA)
+
 
 dataB = scipy.io.loadmat(file_B, matlab_compatible=True)
 
 PImeansB = dataB["PImeans"][0]
 PIB = np.asarray([data[0] for data in dataB["PI"]])
-print(PImeansB)
-print(PIB)
+
 
 dataC = scipy.io.loadmat(file_C, matlab_compatible=True)
 
 PImeansC = dataC["PImeans"][0]
 PIC = np.asarray([data[0] for data in dataC["PI"]])
-print(PImeansC)
-print(PIC)
+
 
 dataD = scipy.io.loadmat(file_D, matlab_compatible=True)
 
 PImeansD = dataD["PImeans"][0]
 PID = np.asarray([data[0] for data in dataD["PI"]])
-print(PImeansD)
-print(PID)
+
 
 
 groups = ["A","B","C","D"]
@@ -47,6 +43,27 @@ indivdata = [PIA, PIB, PIC, PID]
 colors = ["#748189", "#8797a0", "#9bacb7", "#5e77f9"]
 sb.set_palette(sb.color_palette(colors))
 f, axes = plt.subplots(1, 2)
+
+#Using Wilcoxon signed rank test to test for light preference
+print("Stats:")
+print("Testing Group A preferences for light")
+prefA = scipy.stats.wilcoxon(PIA, y=None, zero_method='wilcox', correction=False)
+print(prefA)
+
+print("Stats:")
+print("Testing Group B preferences for light")
+prefB = scipy.stats.wilcoxon(PIB, y=None, zero_method='wilcox', correction=False)
+print(prefB)
+
+print("Stats:")
+print("Testing Group C preferences for light")
+prefC = scipy.stats.wilcoxon(PIC, y=None, zero_method='wilcox', correction=False)
+print(prefC)
+
+print("Stats:")
+print("Testing Group D preferences for light")
+prefD = scipy.stats.wilcoxon(PID, y=None, zero_method='wilcox', correction=False)
+print(prefD)
 
 ax1 = sb.boxplot(data=meansdata, ax=axes[0], width=0.5)
 ax1.set(xlabel='', ylabel='light preference index')
